42c7bf0d3f1848ac5a891a13b103cd397436c778
Cantin-L <cantin.liottard.pro@gmail.com>
2023-01-30 16:59:58 +0100

    Je suis un homme

---
+++ humaine.py
@@ -0,0 +1,26 @@
+class humain ():
+
+    def __init__(self):
+        self.nom
+        self.prenom
+        self.poste
+        
+    def getnom(self):
+        return self.nom 
+
+    def getprenom(self):
+        return self.prenom 
+
+    def getposte(self):
+        return self.poste 
+    
+    def setnom(self,n):
+        self.nom = n
+
+    def setnom(self,p):
+        self.prenom = p
+
+    def setnom(self,p):
+        self.poste = p
+
+#ordres marcher supervision
